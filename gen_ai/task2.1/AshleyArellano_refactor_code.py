50b47d815735ac2e3199ef8999cdf5f548c00976
asterid-leaper <asteridleaper@gmail.com>
2025-02-21 23:18:24 -0800

    Added the task 2.1 and task 2.2 files
    -Added the new refactored code in task 2.1
    -Add a blank task 2.2 file

---
+++ gen_ai/task2.1/AshleyArellano_refactor_code.py
@@ -0,0 +1,28 @@
+#This is the refactored code to a short python script
+#that reads files and get the reverse complement of a DNA strand
+def check_file():
+    while True:
+        try:
+            fname = input("Enter the file name: \n")
+            with open(fname, 'r') as _:
+                print("File accepted. Loading...")
+                return fname
+        except IOError:
+            print("File not accessible. Try again.\n")
+
+
+def read_file(fname):
+    with open(fname, 'r') as file:
+        for line in file:
+            revc_line = reverse_complement(line.strip())  # Remove unnecessary whitespace
+            print(revc_line)
+
+
+def reverse_complement(dna_sequence):
+    complement_map = str.maketrans("ATGC", "TACG")
+    return dna_sequence.translate(complement_map)[::-1]  # Translate and reverse in one step
+
+
+# Main Function
+fname = check_file()
+read_file(fname)
