5b436b514c7cfdde13d724e99d9b06a5c3c96bc3
asterid-leaper <asteridleaper@gmail.com>
2025-02-21 23:33:40 -0800

    Updated AshleyArellano_refactor_code.py for task 2.1 with refactored code from ChatGPT

--- gen_ai/task2.1/AshleyArellano_refactor_code.py
+++ gen_ai/task2.1/AshleyArellano_refactor_code.py
@@ -1,54 +1,28 @@
-#This is the code to a short python script
+#This is the re-factored code to a short python script
 #that reads files and get the reverse complement of a DNA strand
-def checkFile(): 
+def check_file():
     while True:
         try:
             fname = input("Enter the file name: \n")
-            iFile = open(fname)
+            with open(fname, 'r') as _:
+                print("File accepted. Loading...")
+                return fname
         except IOError:
             print("File not accessible. Try again.\n")
-            continue
-        else:
-            iFile.close()
-            print("File accepted. Loading...")
-            return fname
 
-def readFile(fname):
-    with open(fname, 'r') as iFile:
-        for line in iFile:
-            singLine = " "
-            singLine = line
-            REVCline = revComplement(singLine)
-            print(REVCline)
-    iFile.close()
 
-def revComplement(line):
-    revLine = ""
-    DNAbase = ""
-    count = 0
-    start = 1
-    end = 0
-    while(count < len(line)):
-        DNAbaseLoc = slice(len(line)-start, len(line)-end) 
-       
-        DNAbase = line[DNAbaseLoc]
-        match DNAbase:
-            case 'A':
-                DNAbase = DNAbase.replace('A', 'T')
-            case 'T':
-                DNAbase = DNAbase.replace('T', 'A')
-            case 'C':
-                DNAbase = DNAbase.replace('C', 'G')
-            case 'G':
-                DNAbase = DNAbase.replace('G', 'C')
+def read_file(fname):
+    with open(fname, 'r') as file:
+        for line in file:
+            revc_line = reverse_complement(line.strip())  # Remove unnecessary whitespace
+            print(revc_line)
 
-        revLine = revLine + DNAbase
 
-        count += 1
-        start += 1
-        end += 1
-    return revLine
+def reverse_complement(dna_sequence):
+    complement_map = str.maketrans("ATGC", "TACG")
+    return dna_sequence.translate(complement_map)[::-1]  # Translate and reverse in one step
 
-#Main Function
-fname = checkFile()
-readFile(fname)
+
+# Main Function
+fname = check_file()
+read_file(fname)
